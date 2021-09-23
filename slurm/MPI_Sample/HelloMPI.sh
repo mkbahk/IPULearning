ce69b47e307b94e028cde7e44f56547b15523dfe
mkbahk <mkbahk@gmail.com>
2021-09-24 07:36:21 +0900

    Remove kubernetes & Slurm

--- slurm/MPI_Sample/HelloMPI.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH -J mpi
-#SBATCH -o mpi_%j.out
-#SBATCH -e mpi_%j.err
-## 4 processes & 2 processes/node
-#SBATCH -n 10
-#SBATCH --tasks-per-node=4
-mpirun ./HelloMPI
--- slurm/OpenMP_Sample/OpenMP.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <omp.h>
-void main() 
-{
-#pragma omp parallel
-{
-    printf("tid=%d\n", omp_get_thread_num());
-}
-}
