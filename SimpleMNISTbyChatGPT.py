583a535305d39ec37ec127b18d77632d5025606c
박문기(Moon-Kee Bahk) <30949880+mkbahk@users.noreply.github.com>
2023-01-21 09:11:24 +0900

    Create SimpleMNISTbyChatGPT.py

---
+++ SimpleMNISTbyChatGPT.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# Load the MNIST dataset
+mnist = keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize the data
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Define the model architecture
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print('Test accuracy:', test_acc)
+
+#End of Codes
